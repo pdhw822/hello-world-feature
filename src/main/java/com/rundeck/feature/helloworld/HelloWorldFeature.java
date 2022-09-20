a908b483429be634de4aa357c48b9d6234f7abb8
Stephen Joyner <stephen@rundeck.com>
2022-09-20 11:55:08 -0500

    Update spring configuration

--- src/main/java/com/rundeck/feature/helloworld/HelloWorldFeature.java
+++ src/main/java/com/rundeck/feature/helloworld/HelloWorldFeature.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.Optional;
 
 @Component
-@EnableAutoConfiguration
 public class HelloWorldFeature implements Feature<HelloWorldFeatureConfig> {
     boolean enabled = true;
     boolean running = false;
